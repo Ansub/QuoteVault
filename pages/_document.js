ce99dd380435d883f16799751b1d7a3c38eff7f7
Ansub <khanansub3@gmail.com>
2023-01-17 11:35:50 +0530

    added meta tags in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,34 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/** Meta Data */}
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Elevate your thoughts, Inspire the World."
+        />
+        <meta property="og:site_name" content="quote-vault.vercel.app" />
+        <meta
+          property="og:description"
+          content="Elevate your thoughts, Inspire the World."
+        />
+        <meta property="og:title" content="Quote Vault" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Quote Vault" />
+        <meta
+          name="twitter:description"
+          content="Elevate your thoughts, Inspire the World."
+        />
+        <meta
+          property="og:image"
+          content="https://quote-vault.vercel.app/images/quotevault.png"
+        />
+        <meta
+          name="twitter:image"
+          content="https://quote-vault.vercel.app/images/quotevault.png"
+        />
+
+        {/** Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
